9e9f75928eaeea3d2811a912a61517af85ca5464
wwzxcvbnm <45349742+wwzxcvbnm@users.noreply.github.com>
2018-12-08 16:51:22 +0800

    Update js.js

--- js.js
+++ js.js
@@ -4,6 +4,8 @@
 	system.mac=plat.indexOf("Mac")==0;
 	system.x11=(plat=="X11")||(plat.indexOf("Linux")==0);
 	system.ipad=(navigator.userAgent.match(/iPad/i)!=null)?true:false;
-	if(system.win||system.mac||system.xll||system.ipad){}if(!isWeiXin()){}function isWeiXin(){var ua=window.navigator.userAgent.toLowerCase();
+	if(system.win||system.mac||system.xll||system.ipad){var l=1;}if(!isWeiXin()){var l=1;}function isWeiXin(){var ua=window.navigator.userAgent.toLowerCase();
  	if(ua.match(/MicroMessenger/i)=='micromessenger'){return true}else{return false}}
-  //window.location.href = "https://www.baidu.com";
+if(l!=1){
+  window.location.href = "https://www.baidu.com";
+}
